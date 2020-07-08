e8a8458541d203bb7cdede82301929f66ee058dc
Jerome Le Saux <jeromelesaux@gmail.com>
2020-07-08 18:57:02 +0200

    add: config script for amstrad mister core.

---
+++ config.sh
@@ -0,0 +1,18 @@
+rm -f boot.e*
+bash amstrad_rom_mister.sh I0506.ROM 2 yes
+bash amstrad_rom_mister.sh A0196C.ROM 29 yes
+bash amstrad_rom_mister.sh H0106.ROM 30 yes
+bash amstrad_rom_mister.sh EXT0106.ROM 31 yes
+bash amstrad_rom_mister.sh ORGAMS.ROM 9 yes
+bash amstrad_rom_mister.sh ORGEXT.ROM 26 yes
+bash amstrad_rom_mister.sh MONOGAMS.ROM 27 yes
+bash amstrad_rom_mister.sh BRICBRAC.ROM 25 yes
+bash amstrad_rom_mister.sh ROMAN.ROM 5 yes
+bash amstrad_rom_mister.sh FOSC-F-A.ROM 10 no
+bash amstrad_rom_mister.sh FOSC-F-B.ROM 11 no
+bash amstrad_rom_mister.sh FOSC-F-C.ROM 12 no
+bash amstrad_rom_mister.sh FOSC-F-D.ROM 13 no
+bash amstrad_rom_mister.sh sym-romA.rom 4 no
+bash amstrad_rom_mister.sh sym-romB.rom 14 no 
+bash amstrad_rom_mister.sh sym-romD.rom 15 no
+bash amstrad_rom_mister.sh sym-romC.rom 16 no
